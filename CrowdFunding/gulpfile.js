0c16f02dc52f2977dab47efcfa420c22d3eb8743
Rijvy <rrijvy@gmail.com>
2018-10-22 06:44:34 +0600

    Add navigation and Carousel

---
+++ CrowdFunding/gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp'),
+    fs = require('fs'),
+    sass = require('gulp-sass'),
+    watch = require('gulp-watch');
+
+gulp.task("sass", function () {
+    return gulp.src('./Styles/style.scss')
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task("watch", function () {
+    watch('./Styles/*.scss', function () {
+        gulp.start('sass');
+    });
+});
